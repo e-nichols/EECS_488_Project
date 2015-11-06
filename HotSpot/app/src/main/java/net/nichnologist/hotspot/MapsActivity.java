313c24a331354baa8861e8324daec1d6bec49c42
Dustin Horvath <dustinmhorvath@gmail.com>
2015-11-06 14:56:54 -0600

    Refactor complete. New map page is in place. Much to do.

--- HotSpot/app/src/main/java/net/nichnologist/hotspot/MapsActivity.java
+++ HotSpot/app/src/main/java/net/nichnologist/hotspot/MapsActivity.java
@@ -70,7 +70,7 @@ public class MapsActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps2);
+        setContentView(R.layout.activity_maps);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -146,7 +146,7 @@ public class MapsActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.maps_activity2, menu);
+        getMenuInflater().inflate(R.menu.maps_activity, menu);
         return true;
     }
 
